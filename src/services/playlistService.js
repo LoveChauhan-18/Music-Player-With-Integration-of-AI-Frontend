3cc19c325d182608147f610496280f2bd619c066
Love Kumar Chauhan <lovechauhan6564@gmail.com>
2026-04-08 15:31:06 +0530

    feat: point frontend api to live render backend url

--- src/services/playlistService.js
+++ src/services/playlistService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://pittu-music-api.onrender.com/api';
 
 // Helper to get JWT headers
 const getAuthHeaders = () => {
